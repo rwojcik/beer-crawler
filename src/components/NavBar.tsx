2dcecbeb365de460d063b230d116df5730d722b9
Rafał Wójcik <rafalwojcik3@gmail.com>
2019-02-05 16:11:05 +0100

    Introduce navbar and stylecheck with tslint

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+
+const NavBar = () => {
+    return(
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="title" color="inherit">
+                    BeerCrawler
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default NavBar;
