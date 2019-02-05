20a06076cfef403fd32af2155c3bfed96d68c3ce
Rafał Wójcik <rafalwojcik3@gmail.com>
2019-02-05 17:47:10 +0100

    Add redux dev tools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { rootReducer } from "./rootReducer";
 
 export function configureStore() {
- return createStore(
-  rootReducer,
-   applyMiddleware(thunk),
- );
+  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 }
