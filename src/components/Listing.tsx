5b15497cb259aad790cb2f9b518d916ea8242185
Rafał Wójcik <rafalwojcik3@gmail.com>
2019-02-06 04:29:36 +0100

    Add styling and example style usage in Listing

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -1,9 +1,40 @@
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles
+} from "@material-ui/core/styles";
 import React from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { IApplicationState } from "../store";
 import { fetchStart } from "../store/beer/beerActions";
 import { IBeer } from "../store/beer/beerTypes";
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      padding: theme.spacing.unit * 2,
+      flexGrow: 1
+    },
+    paper: {
+      padding: theme.spacing.unit * 2,
+      margin: "auto",
+      maxWidth: 500
+    },
+    image: {
+      width: 128,
+      height: 128
+    },
+    img: {
+      margin: "auto",
+      display: "block",
+      maxWidth: "100%",
+      maxHeight: "100%"
+    }
+  });
 
 interface IPropsFromState {
   data: IBeer[];
@@ -21,25 +52,33 @@ interface IOtherProps {
   children?: (props: ListingContainerProps) => React.ReactNode;
 }
 
-export class Listing extends React.Component<ListingContainerProps & IOtherProps> {
-
+export class Listing extends React.Component<
+  ListingContainerProps & IOtherProps & WithStyles<typeof styles>
+> {
   public componentDidMount() {
     this.props.fetchStart(1);
   }
 
   public render() {
+    const { classes } = this.props;
     return (
-      <React.Fragment>
-        Here should be listing
-      </React.Fragment>
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Grid container>
+            <Grid item>item</Grid>
+          </Grid>
+        </Paper>
+      </div>
     );
   }
 }
 
+const StyledListing = withStyles(styles)(Listing);
+
 const mapStateToProps = ({ beers }: IApplicationState) => ({
   data: beers.data,
   errors: beers.errors,
-  loading: beers.loading,
+  loading: beers.loading
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
@@ -49,4 +88,4 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
 export const ListingComponent = connect(
   mapStateToProps,
   mapDispatchToProps
-)(Listing);
+)(StyledListing);
