66ca8d724bc34a7106f999b550fbdf54202d37c8
Rafał Wójcik <rafalwojcik3@gmail.com>
2019-02-15 19:22:30 +0100

    Cleanup: comments, unused css

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,14 +4,10 @@ import { beersReducer } from "./beer/beerReducer";
 import { beersSaga } from "./beer/beerSagas";
 import { IBeersState } from "./beer/beerTypes";
 
-// The top-level state object.
 export interface IApplicationState {
   beers: IBeersState;
 }
 
-// Whenever an action is dispatched, Redux will update each top-level application state property
-// using the reducer with the matching name. It's important that the names match exactly, and that
-// the reducer acts on the corresponding IApplicationState property type.
 export const rootReducer = combineReducers<IApplicationState>({
   beers: beersReducer,
 });
